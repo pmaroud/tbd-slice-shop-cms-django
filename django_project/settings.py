0184658712dde2ec994b077723963fece1b5347b
Pantelis Maroudis <pantelis.maroudis@slicelife.com>
2025-01-22 15:14:43 +0200

    add homepage

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local
+    "pages.apps.PagesConfig",  # new
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],  # new
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
